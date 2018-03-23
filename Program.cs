0684139863a87782448ba71c0d1c59adfe09d701
Ostrander, Mathew <mathew.ostrander@gmail.com>
2018-03-23 14:46:20 -0500

    Iterating and printing to the console

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace wordyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] letters = { 'a', 'b', 'c', 'd', 'e', 'f' };
+            
+            for (int i = 0; i < letters.Length; i++)
+            {
+                Console.WriteLine(letters[i]);
+
+
+                for (int j = 0; j < letters.Length; j++)
+                {
+                    if (j != i )
+                    {
+                        Console.WriteLine(letters[i].ToString() + letters[j].ToString());
+
+                        for (int k = 0; k < letters.Length; k++)
+                        {
+                            if (k != i && k != j)
+                            {
+                                Console.WriteLine(letters[i].ToString() + letters[j].ToString() + letters[k].ToString());
+                            }
+
+
+                        }
+
+                    }
+                }
+            }
+
+            Console.WriteLine("End!");
+        }
+
+        public static void WriteOthers(char[] thing, int baseIndex)
+        {
+            string outputString = "";
+
+            for (int i = 0 ; i < thing.Length; i++)
+            {
+                if (i != baseIndex)
+                {
+                   outputString += thing[i].ToString();
+                }
+            }
+            Console.WriteLine(outputString);
+
+        }
+
+    }
+}
